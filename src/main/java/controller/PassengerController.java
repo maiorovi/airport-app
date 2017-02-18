fd2afb367115b6f97cb8e091bc32bf7b3eee0dfd
Iegor Maiorov <maiorov.prog@gmail.com>
2017-02-18 20:35:20 +0200

    created basic structure of app which will be used on course about spring

--- src/main/java/controller/PassengerController.java
+++ src/main/java/controller/PassengerController.java
@@ -6,14 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import repository.PassengerRepository;
 
 @Controller
-public class TestController {
+public class PassengerController {
 	@Autowired
 	PassengerRepository passengerRepository;
 
-	@RequestMapping(path="test")
+	@RequestMapping(path="passengers")
 	@ResponseBody
 	public String requestMapping() {
 		passengerRepository.save(new Passenger("Egor", "Maiorov"));
